ba742a0c0fe28057e4e06eb4ec1c185e035cd0e8
Allawi <allawi465@gmail.com>
2023-03-22 13:34:43 +0100

    added GlobalStyle and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Theme from './style/theme';
+import GlobalStyle from './style/GlobalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Theme>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Theme>
+  </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
