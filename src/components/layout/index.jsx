64ae7f9ff223d8c6dfc7dbb103582601554b2322
Allawi <allawi465@gmail.com>
2023-03-30 00:10:09 +0200

    format file

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -3,10 +3,9 @@ import Footer from './footer';
 import Header from './header';
 import { Outlet } from 'react-router-dom';
 
-
 function Layout() {
-    return (
-      <div className="wrapper">
+  return (
+    <div className="wrapper">
       <Header />
       <div className="content">
         <Outlet />
