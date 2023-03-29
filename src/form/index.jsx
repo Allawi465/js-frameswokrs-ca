790a0457e2914d5835f3e36f043f4e134722f6e7
Allawi <allawi465@gmail.com>
2023-03-30 00:12:13 +0200

    added index file for form folder

---
+++ src/form/index.jsx
@@ -0,0 +1,4 @@
+export {default as AddProductsToCart } from './cart/addToCart';
+export {default as RemoveProducts } from './cart/removeCart';
+export {default as ContactForm } from './contact/index';
+export {default as Search } from './search/index';
