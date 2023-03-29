b47bb831a31f9c8c74542f3a9cc4af3fabcd08fe
Allawi <allawi465@gmail.com>
2023-03-30 00:11:58 +0200

    format file

---
+++ src/components/cart/context/index.jsx
@@ -0,0 +1,5 @@
+export {default as AddProduct } from './addProduct';
+export {default as CartContext } from './cartContext';
+export {default as CartProvider } from './cartProvider';
+export {default as RemoveProduct } from './removeProducts';
+export {default as UpdateProduct } from './updateCart';
