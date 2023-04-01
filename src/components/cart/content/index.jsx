e2b58ac3709b7a0043a47854212c3deb960c005a
Allawi <allawi465@gmail.com>
2023-04-01 05:51:19 +0200

    imported tax and shipping from index file

--- src/components/cart/content/index.jsx
+++ src/components/cart/content/index.jsx
@@ -2,3 +2,4 @@ export {default as EmptyCart } from './emptyCart'
 export {default as CartHeading } from './cartHeading'
 export {default as GetCartProducts } from './getCartProducts'
 export {default as OrderSummery } from './orderSummery'
+export {tax, shipping } from './orderSummery';
